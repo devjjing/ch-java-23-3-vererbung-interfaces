8bb153878ffd75eea9c363b73bebd213b4fe2b57
GATA5 <Tangjing.Gan@coop.ch>
2023-12-04 16:31:57 +0100

    Change class name Destination to lowercase

--- src/main/java/org/example/records/Destination.java
+++ src/main/java/org/example/records/Destination.java
@@ -2,7 +2,7 @@ package org.example.records;
 
 import java.math.BigDecimal;
 
-public record destination(
+public record Destination(
         String name,
         BigDecimal price,
         String country
